d57f2d58711a15ab97eca9767448724451722ab4
Subhankar <65107086+devil200120@users.noreply.github.com>
2025-07-15 19:13:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -117,7 +117,6 @@ app.use(cors({
 app.options('*', (req, res) => {
   res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.sendStatus(200);
 });
